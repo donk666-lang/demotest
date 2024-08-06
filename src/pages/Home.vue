4c47d5cd361ad39f84427b703ad4ef9fb4c42abc
mrliu <1502220623@qq.com>
2024-08-06 17:31:27 +0800

    提交

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,68 +1,111 @@
 <template>
   <div class="box">
-    <div class="header">
+    <div class="header" :style="!showMenu ? 'height:44px' : 'height:80px'">
       <div>
-        <img src="../assets/images/logo.png" class="logo">
-        <span class="headerText">塾専用コミュニケーション&業務管理システム</span>
+        <img src="../assets/images/logo.png" class="logo" />
+        <span class="headerText" v-show="showMenuList">
+          塾専用コミュニケーション&業務管理システム
+        </span>
       </div>
-      <div class="nav">
-        <div class="navList">
-          <el-menu default-active="2" mode="horizontal">
-            <el-sub-menu index="1">
-              <template #title>
-                <span>サービス</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="1-1">サービス一覧</el-menu-item>
-                <el-menu-item index="1-2">item one</el-menu-item>
-              </el-menu-item-group>
-            </el-sub-menu>
-            <el-sub-menu index="2">
-              <template #title>
-                <span>基本機能</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="2-1">item one</el-menu-item>
-                <el-menu-item index="2-2">item one</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>item four</template>
-                <el-menu-item index="1-4-1">item one</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="3">
-              <template #title>
-                <span>ヘルプ</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="2-1">item one</el-menu-item>
-                <el-menu-item index="2-2">item one</el-menu-item>
-              </el-menu-item-group>
-            </el-sub-menu>
-          </el-menu>
+      <div class="nav" v-if="showMenu">
+        <div>
+          <menuTemplate :showMenuList="showMenuList" />
         </div>
-        <div class="loginBtn">
-          塾講師ログイン
-        </div>
+        <div class="loginBtn">塾講師ログイン</div>
+      </div>
+      <div
+        v-else
+        :class="{ change: true, clicked: isClicked }"
+        @click="handleClick"
+      >
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
+    <div :class="mainClass">
+      <div class="mainTitle">
+        <h1>その他の登録</h1>
+      </div>
+      <div class="oneList" :class="{ vertical: !showMenu }">
+        <div class="oneListMain" :class="{ vertical: !showMenu }">
+          <p>メールアドレス（共通アカウント）でログインする場合はこちら</p>
+          <div class="oneListBottom">
+            <el-button color="#fddc3e">共通アカウントログイン</el-button>
+          </div>
+        </div>
+        <div class="oneListMain" :class="{ vertical: !showMenu }">
+          <p>
+            Comiruをすでに友達に追加済みの方
+            以下のアイコンをクリックしてログイン
+          </p>
+          <div class="oneListBottom">
+            <img src="../assets/images/btn_base.png" class="btnBase" />
+          </div>
+        </div>
+        <div class="oneListMain" :class="{ vertical: !showMenu }" style="border-right: none;">
+          <p>メールアドレス（共通アカウント）でログインする場合はこちら</p>
+          <div class="oneListBottom">
+            <img src="../assets/images/ja.png" class="ja" />
+          </div>
+        </div>
+      </div>
+      <div class="twoList">
+        <div class="mainTitle">
+        <h1>生徒・保護者ログイン（スクールポパー）</h1>
+      </div>
+      <p>
+        生徒番号とパスワードを入力してログインしてください。生徒番号やパスワードパスワードがわからない場合は、教室にお問い合わせください。
+      </p>
+    <div>
+      123
+    </div>
+      </div>
+    </div>
+    <div class="mainApp"></div>
   </div>
 </template>
-  
-<script setup>
-import { onMounted, ref } from 'vue';
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from '@element-plus/icons-vue'
+
+<script lang="ts" setup>
+import { onMounted, ref, onUnmounted, computed } from "vue";
+import menuTemplate from "../components/nav.vue";
+
+const windowWidthThreshold = 1524;
+const showMenuList = ref(true);
+const showMenu = ref(true);
+const isClicked = ref(false);
+
+const handleClick = () => {
+  isClicked.value = !isClicked.value;
+};
+
+const handleResize = () => {
+  if (window.innerWidth <= windowWidthThreshold) {
+    showMenuList.value = false;
+  } else {
+    showMenuList.value = true;
+  }
+  if (window.innerWidth <= 1047) {
+    showMenu.value = false;
+  } else {
+    showMenu.value = true;
+  }
+};
 
 onMounted(() => {
+  handleResize();
+  window.addEventListener("resize", handleResize);
+});
 
+onUnmounted(() => {
+  window.removeEventListener("resize", handleResize);
+});
+
+const mainClass = computed(() => {
+  return showMenu.value ? "main" : "main vertical";
 });
 </script>
-  
+
 <style lang="scss" scoped>
 * {
   margin: 0;
@@ -74,17 +117,15 @@ onMounted(() => {
   height: 100vh;
 
   .header {
-    height: 80px;
     background: #fff;
-    box-shadow: 0px 1px 2px rgba(0, 0, 0, .2);
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0 40px;
 
     .logo {
-      width: 172px;
-      height: 40px;
+      width: 30%;
     }
 
     .headerText {
@@ -96,12 +137,8 @@ onMounted(() => {
 
     .nav {
       display: flex;
-
-      .navList {
-        width: 410px;
-      }
-
       .loginBtn {
+        width: 115px;
         height: 32px;
         padding: 0 10px;
         border-radius: 16px;
@@ -109,12 +146,165 @@ onMounted(() => {
         text-align: center;
         line-height: 32px;
         font-weight: bold;
+        position: relative;
+        right: 20px;
+        top: 10px;
+      }
+    }
+  }
+
+  .main {
+    width: 65%;
+    margin: 20px auto;
+
+    &.vertical {
+      width: 96%;
+    }
+
+    .mainTitle {
+      border-bottom: 1px solid #eee;
+      padding: 10px 0;
+      width: 100%;
+    }
+
+    .oneList {
+      display: flex;
+      padding: 10px 0;
+
+      &.vertical {
+        display: flex;
+        flex-direction: column;
+      }
+
+      .oneListMain {
+        width: 30%;
+        flex: 1;
+        border-right: 2px solid #b4a094;
+        padding: 0 8px;
+        &.vertical {
+          width: 100%;
+          border-right: none;
+          border-bottom: 2px solid #b4a094;
+          padding: 16px 0;
+        }
+        &:last-child {
+          border-right: none;
+          border-bottom: none;
+        }
+
+        p {
+          line-height: 30px;
+          text-align: center;
+        }
+      }
+
+      .oneListBottom {
+        text-align: center;
+
+        .btnBase {
+          width: 36px;
+          height: 36px;
+        }
+
+        .ja {
+          width: 96px;
+          height: 36px;
+        }
+      }
+    }
+
+    .twoList {
+      p {
+        margin: 10px 0;
       }
     }
   }
 }
 
+.change {
+  width: 50px;
+  height: 50px;
+  transform: scale(0.5);
+}
+
+.change > div {
+  width: 50px;
+  height: 5px;
+  background-color: black;
+  position: absolute;
+  transition: all 0.2s linear;
+}
+
+.change > div:nth-of-type(1) {
+  top: 3px;
+}
+
+.change > div:nth-of-type(2) {
+  top: 20px;
+}
+
+.change > div:nth-of-type(3) {
+  top: 36px;
+}
+
+.change.clicked div:nth-of-type(1) {
+  top: 20px;
+  background-color: black;
+  transform: rotate(45deg);
+}
+
+.change.clicked div:nth-of-type(2) {
+  background-color: black;
+  transform: scaleX(0);
+}
+
+.change.clicked div:nth-of-type(3) {
+  top: 20px;
+  background-color: black;
+  transform: rotate(-45deg);
+}
+
 .el-menu--horizontal.el-menu {
   border-bottom: none;
-}</style>
-  
+}
+
+/* Media queries for responsive text size */
+@media (max-width: 1047px) {
+  .headerText,
+  .oneListMain p,
+  .twoList p {
+    font-size: 14px;
+  }
+
+  .loginBtn {
+    font-size: 14px;
+    padding: 0 8px;
+  }
+}
+
+@media (max-width: 768px) {
+  .headerText,
+  .oneListMain p,
+  .twoList p {
+    font-size: 12px;
+  }
+
+  .loginBtn {
+    font-size: 12px;
+    padding: 0 6px;
+  }
+}
+
+@media (max-width: 480px) {
+  .headerText,
+  .oneListMain p,
+  .twoList p {
+    font-size: 10px;
+  }
+
+  .loginBtn {
+    font-size: 10px;
+    padding: 0 4px;
+  }
+}
+</style>
